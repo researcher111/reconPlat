d6985d20864d7db6944180196269d8342c550d63
Daniel <dggraham@cs.wm.edu>
2014-07-12 12:44:06 -0400

    Software stuff

---
+++ Software/ccsworkspace/SmartWatch/Debug/PCD8544.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+PCD8544.obj: ../PCD8544.c
+PCD8544.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
+PCD8544.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
+PCD8544.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+PCD8544.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+PCD8544.obj: ../PCD8544.h
+
+../PCD8544.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
+../PCD8544.h: 
---
+++ Software/ccsworkspace/SmartWatch2/Debug/ADC_and_LCD.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+ADC_and_LCD.obj: ../ADC_and_LCD.c
+ADC_and_LCD.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
+ADC_and_LCD.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2452.h
+ADC_and_LCD.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+ADC_and_LCD.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+ADC_and_LCD.obj: ../PCD8544.h
+
+../ADC_and_LCD.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2452.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
+../PCD8544.h: 
